a828fe0733380d1daa58a94ece5be7925fa8b77e
chamikaJ <chamika@ceydigital.com>
2024-05-17 12:09:01 +0530

    docs: updated SETUP_THE_PROJECT.md, removed secret files from .env

--- worklenz-backend/jest.config.js
+++ worklenz-backend/jest.config.js
@@ -11,7 +11,7 @@ module.exports = {
   // bail: 0,
 
   // The directory where Jest should store its cached dependency information
-  // cacheDirectory: "C:\\Users\\dinindu\\AppData\\Local\\Temp\\jest",
+  // cacheDirectory: "",
 
   // Automatically clear mock calls and instances between every test
   clearMocks: true,
