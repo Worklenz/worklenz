8c526f7eff3f1c93ba082997170854a0aa698943
kithmina1999 <kithgaming99@gmail.com>
2025-03-05 09:17:46 +0530

    Revert: "fix: correct SQL privilege order and grant schema usage" due to conflict in role creation

--- worklenz-backend/database/6_user-permission.sql
+++ worklenz-backend/database/6_user-permission.sql
@@ -4,10 +4,10 @@
 
 REVOKE CREATE ON SCHEMA public FROM PUBLIC;
 CREATE ROLE worklenz_client;
-CREATE USER worklenz_backend WITH PASSWORD 'securepassword123';
-GRANT CONNECT ON DATABASE worklenz_db TO worklenz_client;
 
+GRANT CONNECT ON DATABASE worklenz_db TO worklenz_client;
 GRANT INSERT, SELECT, UPDATE, DELETE ON ALL TABLES IN SCHEMA public TO worklenz_client;
+
 GRANT USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO worklenz_client;
 
 REVOKE ALL PRIVILEGES ON task_priorities FROM worklenz_client;
@@ -31,4 +31,5 @@ GRANT SELECT ON sys_project_statuses TO worklenz_client;
 REVOKE ALL PRIVILEGES ON sys_project_healths FROM worklenz_client;
 GRANT SELECT ON sys_project_healths TO worklenz_client;
 
+CREATE USER worklenz_backend WITH PASSWORD 'securepassword123';
 GRANT worklenz_client TO worklenz_backend;
