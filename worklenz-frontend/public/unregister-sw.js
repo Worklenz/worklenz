fe2518d53c6456405e44be682a1d5b70c99e001e
chamikaJ <chamika@ceydigital.com>
2025-05-14 16:31:17 +0530

    feat(service-worker): add unregister script and update index.html
    - Introduced a new script to unregister service workers, enhancing control over service worker lifecycle.
    - Updated index.html to include the unregister script, ensuring it is loaded for proper service worker management.

---
+++ worklenz-frontend/public/unregister-sw.js
@@ -0,0 +1,7 @@
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.getRegistrations().then(function(registrations) {
+    for(let registration of registrations) {
+      registration.unregister();
+    }
+  });
+} 
