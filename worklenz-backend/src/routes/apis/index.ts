2587b8afd99354cb7e70b5c72d2abaa130b567c5
Omindu Hirushka <102536488+OminduHirushka@users.noreply.github.com>
2025-07-09 07:43:44 +0530

    user activity feed - backend

--- worklenz-backend/src/routes/apis/index.ts
+++ worklenz-backend/src/routes/apis/index.ts
@@ -56,7 +56,10 @@ import billingApiRouter from "./billing-api-router";
 import taskDependenciesApiRouter from "./task-dependencies-api-router";
 
 import taskRecurringApiRouter from "./task-recurring-api-router";
-import customColumnsApiRouter from "./custom-columns-api-router";
+
+import customColumnsApiRouter from "./custom-columns-api-router";
+
+import userActivityLogsApiRouter from "./user-activity-logs-api-router";
 
 const api = express.Router();
 
@@ -116,5 +119,7 @@ api.use("/task-dependencies", taskDependenciesApiRouter);
 api.use("/task-recurring", taskRecurringApiRouter);
 
 api.use("/custom-columns", customColumnsApiRouter);
+
+api.use("/logs", userActivityLogsApiRouter);
 
 export default api;
