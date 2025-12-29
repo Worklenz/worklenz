f107c6859a16cd79da5703d3e112941484116e66
chamikaJ <chamika@ceydigital.com>
2025-12-29 13:49:05 +0530

    feat: Implement task access verification middleware across API routes

--- worklenz-backend/src/routes/apis/sub-tasks-api-router.ts
+++ worklenz-backend/src/routes/apis/sub-tasks-api-router.ts
@@ -4,11 +4,12 @@ import SubTasksController from "../../controllers/sub-tasks-controller";
 
 import idParamValidator from "../../middlewares/validators/id-param-validator";
 import safeControllerFunction from "../../shared/safe-controller-function";
+import verifyTaskAccess from "../../middlewares/verify-task-access";
 
 const subTasksApiRouter = express.Router();
 
-subTasksApiRouter.get("/names/:id", idParamValidator, safeControllerFunction(SubTasksController.getNames)); // :id = parent task id
-subTasksApiRouter.post("/roadmap/:id", idParamValidator, safeControllerFunction(SubTasksController.getSubTasksRoadMap)); // :id = parent task id
-subTasksApiRouter.get("/:id", idParamValidator, safeControllerFunction(SubTasksController.get)); // :id = parent task id
+subTasksApiRouter.get("/names/:id", idParamValidator, verifyTaskAccess('params', 'id'), safeControllerFunction(SubTasksController.getNames)); // :id = parent task id
+subTasksApiRouter.post("/roadmap/:id", idParamValidator, verifyTaskAccess('params', 'id'), safeControllerFunction(SubTasksController.getSubTasksRoadMap)); // :id = parent task id
+subTasksApiRouter.get("/:id", idParamValidator, verifyTaskAccess('params', 'id'), safeControllerFunction(SubTasksController.get)); // :id = parent task id
 
 export default subTasksApiRouter;
--- worklenz-backend/src/routes/apis/task-dependencies-api-router.ts
+++ worklenz-backend/src/routes/apis/task-dependencies-api-router.ts
@@ -1,11 +1,12 @@
 import express from "express";
 
 import TaskdependenciesController from "../../controllers/task-dependencies-controller";
+import verifyTaskAccess, {verifyTaskAccessViaDependency} from "../../middlewares/verify-task-access";
 
 const taskDependenciesApiRouter = express.Router();
 
-taskDependenciesApiRouter.post("/", TaskdependenciesController.saveTaskDependency);
-taskDependenciesApiRouter.get("/:id", TaskdependenciesController.getTaskDependencies);
-taskDependenciesApiRouter.delete("/:id", TaskdependenciesController.deleteById);
+taskDependenciesApiRouter.post("/", verifyTaskAccess('body', 'task_id'), TaskdependenciesController.saveTaskDependency);
+taskDependenciesApiRouter.get("/:id", verifyTaskAccess('params', 'id'), TaskdependenciesController.getTaskDependencies);
+taskDependenciesApiRouter.delete("/:id", verifyTaskAccessViaDependency('params', 'id'), TaskdependenciesController.deleteById);
 
 export default taskDependenciesApiRouter;
