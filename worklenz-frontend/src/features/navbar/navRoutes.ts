360ed5f6a29357e86e821f18a134f2f5cdd77ce5
shancds <shancds1@gmail.com>
2026-04-24 13:18:26 +0530

    Fix: Database changes and rename the nav bar visible name

--- worklenz-frontend/src/features/navbar/navRoutes.ts
+++ worklenz-frontend/src/features/navbar/navRoutes.ts
@@ -34,7 +34,7 @@ export const navRoutes: NavRoutesType[] = [
     freePlanFeature: false,
   },
   {
-    name: 'my-team-reports',
+    name: 'reports',
     path: '/worklenz/team-lead-reports',
     adminOnly: false,
     freePlanFeature: true,
