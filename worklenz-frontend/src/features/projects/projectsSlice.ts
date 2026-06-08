e4adae6b071f14354e466bdc0ce4c8fee084b044
DinuniWanniarachchi-work <dinuni@ceydigital.com>
2026-06-08 12:40:13 +0530

    fix(projects): set default sorting parameters for project fetching

--- worklenz-frontend/src/features/projects/projectsSlice.ts
+++ worklenz-frontend/src/features/projects/projectsSlice.ts
@@ -8,6 +8,9 @@ import { IProjectManager } from '@/types/project/projectManager.types';
 import { IGroupedProjectsViewModel } from '@/types/project/groupedProjectsViewModel.types';
 import { ProjectGroupBy } from '@/types/project/project.types';
 
+const DEFAULT_PROJECT_SORT_FIELD = 'name';
+const DEFAULT_PROJECT_SORT_ORDER = 'ascend';
+
 interface ProjectState {
   projects: {
     data: IProjectViewModel[];
@@ -79,8 +82,8 @@ const initialState: ProjectState = {
   requestParams: {
     index: 1,
     size: DEFAULT_PAGE_SIZE,
-    field: '',
-    order: '',
+    field: DEFAULT_PROJECT_SORT_FIELD,
+    order: DEFAULT_PROJECT_SORT_ORDER,
     search: '',
     filter: 0,
     statuses: null,
